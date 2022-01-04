1ec5fb2589175afabeaeb2bfa38c1aa62a6d1d57
abid-novel <abidnovel03@gmail.com>
2022-01-05 01:42:58 +0600

    Java_Basic_Third_Assignment_Task_3

---
+++ src/Assignment3/Task_3.java
@@ -0,0 +1,33 @@
+package Assignment3;
+
+//Task_3:  Find the max and min number from a given array
+
+
+public class Task_3 {
+    public static void main(String[] args) {
+        int arr[] = {2,5,10,3,55,45,88,20,0,5,9};
+        System.out.println("Maximum value is: "+getMaxValue(arr));
+        System.out.println("Minimum value is: "+getMinValue(arr));
+
+    }
+
+     public static int getMaxValue(int numbers[]){
+        int maxValue = numbers[0];
+        for (int i =1; i<numbers.length; i++) {
+            if(numbers[i]>maxValue){
+                maxValue = numbers[i];
+            }
+        }
+        return maxValue;
+    }
+
+    public static int getMinValue(int numbers[]){
+        int minValue = numbers[0];
+        for (int i =1; i<numbers.length; i++) {
+            if(numbers[i]<minValue){
+                minValue = numbers[i];
+            }
+        }
+        return minValue;
+    }
+}
