2261af1339bb5ccae9a21545c437a7504910255a
abid-novel <abidnovel03@gmail.com>
2022-01-11 17:43:49 +0600

    Java_Basic_Programme_Third_Assignment

---
+++ src/Assignment3/Task_2.java
@@ -0,0 +1,18 @@
+package Assignment3;
+
+//Task_2: Write a program to find if any element is duplicate. If duplicate, show the value.
+
+public class Task_2 {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 2, 7, 8, 8, 3};
+
+        System.out.println("Duplicate elements in given array: ");
+
+        for(int i = 0; i < arr.length; i++) {
+            for(int j = i + 1; j < arr.length; j++) {
+                if(arr[i] == arr[j])
+                    System.out.println(arr[j]);
+            }
+        }
+    }
+}
