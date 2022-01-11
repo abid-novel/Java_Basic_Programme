4bac90d167a6ceb9a09c0c56620996ed96e08836
abid-novel <abidnovel03@gmail.com>
2022-01-11 17:23:10 +0600

    Update

--- src/Assignment3/Task_3.java
+++ src/Assignment3/Task_3.java
@@ -5,13 +5,13 @@ package Assignment3;
 
 public class Task_3 {
     public static void main(String[] args) {
-        int arr[] = {2,5,10,3,55,45,88,20,0,5,9};
+        int[] arr = {2,5,10,3,55,45,88,20,0,5,9};
         System.out.println("Maximum value is: "+getMaxValue(arr));
         System.out.println("Minimum value is: "+getMinValue(arr));
 
     }
 
-     public static int getMaxValue(int numbers[]){
+     public static int getMaxValue(int[] numbers){
         int maxValue = numbers[0];
         for (int i =1; i<numbers.length; i++) {
             if(numbers[i]>maxValue){
@@ -21,7 +21,7 @@ public class Task_3 {
         return maxValue;
     }
 
-    public static int getMinValue(int numbers[]){
+    public static int getMinValue(int[] numbers){
         int minValue = numbers[0];
         for (int i =1; i<numbers.length; i++) {
             if(numbers[i]<minValue){
